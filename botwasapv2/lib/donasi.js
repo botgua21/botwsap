bb7501788fd71df9b4ec31bd40bc4e2eea26bcb5
botgua21 <77573568+botgua21@users.noreply.github.com>
2021-01-20 14:31:31 +0700

    Add files via upload

---
+++ botwasapv2/lib/donasi.js
@@ -0,0 +1,12 @@
+const donasi = () => { 
+	return `       
+
+DONASI BOSS
+
+PULSA:
+083856085455
+DONASI SUBSCRIBE YOUTUBE
+https: '//youtube.com/channel/UCjOtJKGySNfDUvoA2D2c9_A'`
+}
+
+exports.donasi = donasi
